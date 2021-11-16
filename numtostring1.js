0ab71dd9574f7b229194a4542caff063e5211e3e
Parham-S <psaniei@hotmail.com>
2021-11-16 05:42:48 -0500

    initial commit

---
+++ numtostring1.js
@@ -0,0 +1,13 @@
+let testNumber = 5;
+
+function inputNumStr() {
+	if (isNaN(testNumber)) {
+		console.log(testNumber, "is not a number, returning as is.");
+		return testNumber;
+	}
+	{
+		console.log(testNumber, "is a number type, returning as string.");
+		return String(testNumber);
+	}
+}
+console.log(inputNumStr(testNumber));
